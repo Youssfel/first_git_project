2c828a0ea493eaaf6f64d1fb9d0ccce55ca2dd5f
Youssfel <youssefel55.el@gmail.com>
2025-06-14 11:27:10 +0100

    Update HelloCydeo.java

--- src/day01/HelloCydeo.java
+++ src/day01/HelloCydeo.java
@@ -6,6 +6,7 @@ public class HelloCydeo {
         System.out.println("Hello Cydeo");
         // changes added
         //changes added again 
+        System.out.println("Hello nature")
         
     }
 }
